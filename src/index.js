f078f4195be53018046ef81e0836dffe7293873f
Manuvg1907 <manuvgcs26@gmail.com>
2025-08-02 00:29:00 +0530

    Update index.js
     added a Toast message pop up

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-// File: src/index.js
+// npm install react-toastify
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -10,11 +11,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'animate.css/animate.min.css';
 
+// Toastify
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// ✅ Show demo toast message
+toast.success("Welcome to Mushroom Store!", {
+  position: "top-right",
+  autoClose: 3000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+});
 
 root.render(
   <React.StrictMode>
-    <App /> {/* ✅ All routes, including /cart, are inside App.js */}
+    <>
+      <App /> {/* ✅ All routes, including /cart, are inside App.js */}
+      <ToastContainer />
+    </>
   </React.StrictMode>
 );
 
